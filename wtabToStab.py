55bd62820c0ea0c60a6326f09ecf1c12cb38b5df
smnra <smnra@163.com>
2018-04-17 17:01:22 +0800

    画出 POI 点

---
+++ wtabToStab.py
@@ -0,0 +1,80 @@
+import json ,random
+
+
+sstr = '''
+  
+    
+    
+{"normal":[
+	{"title":"第2193章 兴奋 - 医圣传承 作者一念","url":"http://www.yishengchuancheng.net/2194.html","isApp":false,"isNew":false,"fit":"","color":"200,203,162"},
+	{"title":"美剧排行榜前十名 | 2014年美剧推荐 | 最新美剧更新 - 您的高清美剧下载中心","url":"http://www.ttmeiju.com/summary.html","img":"http://hao.weidunewtab.com/m/ttmeiju.com.png","isApp":false,"isNew":false,"fit":"","color":"147,186,212"},
+	{"title":"种子资源 - Powered by Discuz!","url":"http://www.baiduyun.me/forum-100-1.html","isApp":false,"isNew":false,"fit":"lh","color":"191,0,0"},
+	{"title":"搜索结果页 | 人人影视网址大全","url":"http://www.allyingshi.com/index/search/k/%E9%BB%91%E6%9A%97%E7%89%A9%E8%B4%A8/en/Dark%20Matter","img":"http://hao.weidunewtab.com/m/allyingshi.com.png","isApp":false,"isNew":false,"fit":"lh","color":"191,0,0"},
+	{"title":"360云盘 - 云盘分享","url":"http://d7f84f.l67.yunpan.cn/lk/QCKudLUWmbgFt#14052443004953390-0","img":"http://hao.weidunewtab.com/m/d7f84f.l67.yunpan.cn.png","isApp":false,"isNew":false,"fit":"lh","color":"48,131,235"},
+	{"title":"触摸未来 第一季 到 第二季 【网盘下载】 - 全搜集","url":"http://www.quansouji.com/media/open/80143229","img":"http://hao.weidunewtab.com/m/quansouji.com.png","isApp":false,"isNew":false,"fit":"lh","color":"191,0,0"},
+	{"title":"搜索结果页|搜吧 souba.me - 最强影视资源搜索站","url":"http://37souba.com/?page=6&k=The+Big+Bang+Theory","img":"http://hao.weidunewtab.com/m/37souba.com.png","isApp":false,"isNew":false,"fit":"lh","color":"191,0,0"},
+	{"title":"天天美剧5百度云盘资源_天天美剧5百度网盘达人的分享主页_盘找找","url":"http://www.panzz.com/u/1932273937/share/","img":"http://hao.weidunewtab.com/m/panzz.com.png","isApp":false,"isNew":false,"fit":"lh","color":"191,0,0"},
+	{"title":"百度网盘搜索引擎","url":"http://so.baiduyun.me/","isApp":false,"isNew":false,"fit":"lh","color":"191,0,0"},
+	{"title":"百度云论坛|百度网盘论坛|百度论坛","url":"http://www.baiduyun.me/","isApp":false,"isNew":false,"fit":"lh","color":"191,0,0"},
+	{"title":"The Erotic Traveler- 第1页-The Erotic Traveler搜索结果-蜘蛛磁力搜索","url":"http://www.zhizhu.so/search/The%20Erotic%20Traveler_ctime_1.html","isApp":false,"isNew":false,"fit":"lh","color":"55,70,81"},
+	{"title":"字幕库(zimuku) -- 字幕下载网站","url":"http://www.zimuku.net/","isApp":false,"isNew":false,"fit":"","color":"72,133,191"},
+	{"title":"MapBasic程序设计","url":"http://www.nulog.cn/24549.html","isApp":false,"isNew":false,"fit":"lh","color":"191,0,0"},
+	{"title":"MapBasic程序设计_图文_百度文库","url":"http://wenku.baidu.com/link?url=n2kwJx453BHAu3xn5RLnvgPntNzxx0nTlCS8tt53LgJ9vjlhkrC130yWE0GtGp0NKpV9bW0aHRMDDu5_bX55qrv1OGhzQpEnmwnQm7yeumS","isApp":false,"isNew":false,"fit":"","color":"225,6,1"},
+	{"title":"糟糕！网页无法访问。","url":"chrome://se-errors/?&url=http%3A%2F%2Flocalhost%3A1158%2Fem&error=connectionfailure","isApp":false,"isNew":false,"fit":"","color":"200,202,161"},
+	{"title":"只在周末 | 没有下不到的电影","url":"http://www.hi67.cn/","isApp":false,"isNew":false,"fit":"lh","color":"191,0,0"},
+	{"title":"表情好陶醉 – 笑笑笑","url":"http://xxxiao.com/698","img":"http://hao.weidunewtab.com/m/xxxiao.com.png","isApp":false,"isNew":false,"fit":"lh","color":"191,0,0"},
+	{"title":"红色警戒2-心灵终结3.3，一个我见过最精良的红警Mod - 『精品软件区』 - 吾爱破解 - LCG - LSG |安卓破解|病毒分析|破解软件|www.52pojie.cn","url":"http://www.52pojie.cn/thread-568522-1-1.html","img":"http://hao.weidunewtab.com/m/52pojie.cn.png","isApp":false,"isNew":false,"fit":"","color":"2,0,0"},
+	{"title":"上古卷轴5天际性感比基尼合集MOD下载_上古卷轴5天际性感比基尼合集下载_游戏堡","url":"http://www.yxbao.com/bd/62810.html","img":"http://hao.weidunewtab.com/m/yxbao.com.png","isApp":false,"isNew":false,"fit":"lh","color":"244,115,14"},
+	{"title":"上古卷轴5天际半透果体围裙MOD下载_上古卷轴5天际半透果体围裙下载_游戏堡","url":"http://www.yxbao.com/bd/63239.html","img":"http://hao.weidunewtab.com/m/yxbao.com.png","isApp":false,"isNew":false,"fit":"lh","color":"244,115,14"},
+	{"title":"www.icodegame.com","url":"http://www.icodegame.com/","img":"http://hao.weidunewtab.com/m/icodegame.com.png","isApp":false,"isNew":false,"fit":"","color":"200,203,162"},
+	{"title":"CodeCombat中国 - 中国爱好者的 CodeCombat！在游戏中学习编程","url":"http://www.codecombat.cn/play/level/the-raised-sword","isApp":false,"isNew":false,"fit":"lh","color":"70,58,44"},
+	{"title":"Gogo APP 部署 · Issue #4865 · XX-net/XX-Net","url":"https://github.com/XX-net/XX-Net/issues/4865","isApp":false,"isNew":false,"fit":"","color":"120,120,120"},
+	{"title":"pycharm使用github - Voidsky - 博客频道 - CSDN.NET","url":"http://blog.csdn.net/hk2291976/article/details/51159974","isApp":false,"isNew":false,"fit":"lh","color":"191,0,0"},
+	{"title":"Live View","url":"https://2857be443faa.nyxdns.com/doc/page/main.asp","img":"http://hao.weidunewtab.com/m/2857be443faa.nyxdns.com.png","isApp":false,"isNew":false,"fit":"","color":"200,203,162"},
+	{"title":"liuwons/wxBot: Python网页微信API","url":"https://github.com/liuwons/wxBot","isApp":false,"isNew":false,"fit":"","color":"120,120,120"},
+	{"title":"搜索-API文档-开发指南-Web服务 API | 高德地图API","url":"http://lbs.amap.com/api/webservice/guide/api/search/","isApp":false,"isNew":false,"isDel":true},
+	{"title":"我妻子的101次婚姻.2017.[1080P][韩国大尺度-结过100次婚的女人]|美剧库","url":"http://www.meijuku.cn/2807.html","img":"http://hao.weidunewtab.com/m/meijuku.cn.png","isApp":false,"isNew":true}]}  
+    
+    
+    
+    
+'''
+result = json.loads(sstr)
+sites = []
+idGroup = 7
+startnum =508
+for i,site in enumerate(result['normal']) :
+    sTabSite = '''
+    "%s" : {
+        "id": %s,
+        "title": "%s",
+        "url": "%s",
+        "thumbnail": "%s",
+        "ts_created": %s,
+        "visits": 0,
+        "visits_morning": 0,
+        "visits_afternoon": 0,
+        "visits_evening": 0,
+        "visits_night": 0,
+        "position": 999,
+        "idgroup": %s
+    },
+'''
+    print(sTabSite % (str(i+startnum), str(i+startnum), site.get('title',''),site.get('url',''),site.get('img',''), str(19035 + random.randint(1,9999999)), str(idGroup)  ) )
+
+'''
+"6": {
+    "id": 8,
+    "title": "Python3 | 菜鸟教程",
+    "url": "http://www.runoob.com/python3/python3-iterator-generator.html",
+    "thumbnail": "http://www.runoob.com/wp-content/themes/runoob/assets/img/runoob-logo.png",
+    "ts_created": 1523928035,
+    "visits": 2,
+    "visits_morning": 2,
+    "visits_afternoon": 0,
+    "visits_evening": 0,
+    "visits_night": 0,
+    "position": 999,
+    "idgroup": 1
+}
+'''
