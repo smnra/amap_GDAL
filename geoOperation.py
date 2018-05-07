91f5c869419e3085b74dc43a6362987f82cc5064
smnra <smnra@163.com>
2018-05-07 11:59:46 +0800

    根据 polygon 的中心的来判断 amap的 建筑物边界是否错乱

--- geoOperation.py
+++ geoOperation.py
@@ -24,19 +24,19 @@ print(testSR.ExportToPrettyWkt())
 testSR.ImportFromEPSG(4326)#returns 6                #设置坐标系投影
 
 
-def isInvalidBound(pointList):
+def isInvalidBound(ring):
     # 根据传入的列表  创建相邻两个元素的 直线(也就是 多边形的每一条边), 添加到 lines 列表中 ,再分别判断这些直线是否 交叉 Crosses
     lines = []
 
-    for i in range(len(pointList) - 1):
+    for i in range(len(ring) - 1):
         line = ogr.Geometry(ogr.wkbLineString)  # 创建一条折线
-        line.AddPoint(float(pointList[i][0]), float(pointList[i][1]))  # 直线的第一个点
-        line.AddPoint(float(pointList[i + 1][0]), float(pointList[i + 1][1]))  # 直线的第二个点
+        line.AddPoint(float(ring[i][0]), float(ring[i][1]))  # 直线的第一个点
+        line.AddPoint(float(ring[i + 1][0]), float(ring[i + 1][1]))  # 直线的第二个点
         lines.append(line)  # 添加到列表中
 
     line = ogr.Geometry(ogr.wkbLineString)  # 创建最后一个点和第一个点的直线
-    line.AddPoint(float(pointList[-1][0]), float(pointList[-1][1]))
-    line.AddPoint(float(pointList[0][0]), float(pointList[0][1]))
+    line.AddPoint(float(ring[-1][0]), float(ring[-1][1]))
+    line.AddPoint(float(ring[0][0]), float(ring[0][1]))
     lines.append(line)
 
     for i in range(len(lines)):  # 遍历每一条直线
@@ -45,14 +45,82 @@ def isInvalidBound(pointList):
                 return True  # 如果交叉 返回True
     return False  # 默认返回False
 
+def isInvalidBound2(ring):
+    # 根据 polygon 的中心的来判断 amap的 建筑物边界是否错乱
+    # ring  为组成多边形的点的坐标列表 如 :[(0, 0), (0, 10), (10, 10), (10, 0)]
+    # center 为建筑物边界的中心点.
+    polygon = ogr.Geometry(ogr.wkbPolygon)  # 创建多边形
+    ring = ogr.Geometry(ogr.wkbLinearRing)  # 创建一个环 ring
+    for tmpRing in ring:
+        ring.AddPoint(float(tmpRing[0]), float(tmpRing[1]))  # 循环添加点到Ring中
+    if ring[0] != ring[-1]:  # 如果ring的第一个元素和最后一个元素不相等
+        ring.CloseRings()  # 用CloseRings闭合Ring，
+    polygon.AddGeometry(ring)  # 把环 ring添加到  polygon
+    area= polygon.GetArea()
+    center = polygon.Centroid()
+    return False  # 默认返回False
+
+
+class Polygon() :
+    def __init__(self, ring):
+        # 根据 polygon 的中心的来判断 amap的 建筑物边界是否错乱
+        # ring  为组成多边形的点的坐标列表 如 :[(0, 0), (0, 10), (10, 10), (10, 0)]
+        # center 为建筑物边界的中心点.
+        self.polygon = ogr.Geometry(ogr.wkbPolygon)  # 创建多边形
+        self.ring = ogr.Geometry(ogr.wkbLinearRing)  # 创建一个环 ring
+        for tmpRing in ring:
+            self.ring.AddPoint(float(tmpRing[0]), float(tmpRing[1]))  # 循环添加点到Ring中
+        if self.ring[0] != self.ring[-1]:  # 如果ring的第一个元素和最后一个元素不相等
+            self.ring.CloseRings()  # 用CloseRings闭合Ring，
+            self.polygon.AddGeometry(ring)  # 把环 ring添加到  polygon
+
+    def getArea(self):
+        #返回 多边形的面积
+        return  self.polygon.GetArea()
+
+    def getCenter(self):
+        # 返回 多边形的中心点
+        return self.polygon.Centroid()
+
+
+
+
 
 if __name__ =='__main__':
-    boundPoints = [(0, 0), (0, 5), (5, 5), (5,0), (-2,3), (6, 6)]
-    isCross = isInvalidBound(boundPoints)
-    print(isCross)
+    ring = [ [108.920221,34.229893], [108.919324,34.228498],
+             [108.918226,34.228962], [108.91807,34.228619],
+             [108.917882,34.22827], [108.917899,34.228202],
+             [108.918644,34.227877],[108.918532,34.22768],
+             [108.91871,34.227589], [108.918368,34.227061],
+             [108.918225,34.227143], [108.917874,34.227175],
+             [108.917499,34.227166], [108.917472,34.227143],
+             [108.91747,34.226853], [108.917231,34.226879],
+             [108.916865,34.226906], [108.916775,34.226906],
+             [108.9167,34.226872], [108.916647,34.226729],
+             [108.916625,34.226595], [108.916611,34.226479],
+             [108.916589,34.225973], [108.916583,34.225845],
+             [108.915273,34.226421], [108.913974,34.227001],
+             [108.912467,34.227643], [108.91172,34.228003],
+             [108.911351,34.228187], [108.912176,34.22949],
+             [108.913284,34.231225], [108.9134,34.231409],
+             [108.913748,34.231933], [108.91398,34.232289],
+             [108.91468,34.233376], [108.915528,34.234652],
+             [108.915828,34.235148], [108.916176,34.235715],
+             [108.91634,34.235975], [108.918181,34.235185],
+             [108.919077,34.234666], [108.919593,34.234304],
+             [108.919966,34.234129], [108.920601,34.233842],
+             [108.921532,34.233411], [108.921765,34.2333],
+             [108.921939,34.233193], [108.921992,34.233072],
+             [108.92206,34.232919], [108.922026,34.23282],
+             [108.921604,34.232184], [108.9215,34.231932],
+             [108.921029,34.231153], [108.920221,34.229893] ]
 
+    tmpPolygon = Polygon(ring)
+    area = tmpPolygon.getArea()
+    print(area)
 
-
+    center = tmpPolygon.getCenter()
+    print(center)
 
 
 
